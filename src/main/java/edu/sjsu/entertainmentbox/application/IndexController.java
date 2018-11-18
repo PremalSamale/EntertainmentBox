7d593521c63ebe03909755e9d8d4cefffe19b295
Premal Samale <premaldattatray.samale@sjsu.edu>
2018-11-18 13:41:37 -0800

    provided feature of value annotation

--- src/main/java/edu/sjsu/entertainmentbox/application/IndexController.java
+++ src/main/java/edu/sjsu/entertainmentbox/application/IndexController.java
@@ -2,11 +2,14 @@ package edu.sjsu.entertainmentbox.application;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	@RequestMapping("/")
-	public String index() {
-		return "index.jsp";
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index.jsp");
+		return mv;
 	}
 }
