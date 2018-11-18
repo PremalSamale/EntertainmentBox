7bd1fd05b9bec4c77f2dd8db0f942084886b981f
Premal Samale <premaldattatray.samale@sjsu.edu>
2018-11-18 00:18:31 -0800

    converted to springBoot project

---
+++ src/main/java/edu/sjsu/entertainmentbox/application/EntertainmentBoxApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.entertainmentbox.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentBoxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/sjsu/entertainmentbox/application/IndexController.java
@@ -0,0 +1,12 @@
+package edu.sjsu.entertainmentbox.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+}
