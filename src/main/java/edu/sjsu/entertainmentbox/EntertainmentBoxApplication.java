0e9d97109a830a72a34002872b203f47d4272f6d
Premal Samale <premaldattatray.samale@sjsu.edu>
2018-11-26 01:58:44 -0800

    updated SpringBootServletInitializer

--- src/main/java/edu/sjsu/entertainmentbox/EntertainmentBoxApplication.java
+++ src/main/java/edu/sjsu/entertainmentbox/EntertainmentBoxApplication.java
@@ -3,15 +3,16 @@ package edu.sjsu.entertainmentbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
-public class EntertainmentBoxApplication {
+public class EntertainmentBoxApplication extends SpringBootServletInitializer{
 	
-	 
+	 @Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(EntertainmentBoxApplication .class);
 	}
